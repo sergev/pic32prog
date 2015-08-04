03620859844bb118ad2d11f74eb9910c188f663c
robert rozee <rozee@mail.com>
2015-08-05 02:48:52 +1200

    minor bugfix to adapter-bitbang.c that does not affect operation. this fix does not warrant recompiling at this time. also a test of how to use Git Gui.

--- adapter-bitbang.c
+++ adapter-bitbang.c
@@ -980,7 +980,7 @@ adapter_t *adapter_open_bitbang (const char *port, int baud_rate)
         buffer[0] = STK_ENTER_PROGMODE;                 // enter program mode (not needed)
         buffer[1] = CRC_EOP;
         serial_write (buffer, 2);
-        serial_read (buffer, 2);
+        n = serial_read (buffer, 2);
 
         if ((n != 2) || (buffer[0] != STK_INSYNC) || (buffer[1] != STK_OK)) {
             fprintf (stderr, "Failed to enter program mode\n");
