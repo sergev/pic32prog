sergev/pic32prog

commit 7d512c6e5c7953c301a0bfe6d5a6a168be433555

Rework mpsse_reset function into mpsse_setPins. Preparation for ICSP

pic32.h
@@ -24,6 +24,12 @@ extern const unsigned pic32_pemx3[];
 extern const unsigned pic32_pemz[];
 extern const unsigned pic32_pemm[];
 
+#define FAMILY_MX1	0
+#define FAMILY_MX3	1
+#define FAMILY_MZ	2
+#define FAMILY_MK	3
+#define FAMILY_MM	4
+
 /*
  * TAP instructions (5-bit).
  */

target.c
@@ -30,16 +30,16 @@ extern print_func_t print_mm;
  */
                     /*-Boot-Devcfg--Row---Print------Code--------Nwords-Version-*/
 static const
-family_t family_mm  = { "mm",
+family_t family_mm  = { "mm", FAMILY_MM, 
                         4, 0x1780,  256, print_mz,  pic32_pemm,  2000, 0x0510 };
 static const
-family_t family_mx1 = { "mx1",
+family_t family_mx1 = { "mx1", FAMILY_MX1,
                         3,  0x0bf0, 128,  print_mx1, pic32_pemx1, 422,  0x0301 };
 static const
-family_t family_mx3 = { "mx3",
+family_t family_mx3 = { "mx3", FAMILY_MX3,
                         12, 0x2ff0, 512,  print_mx3, pic32_pemx3, 1044, 0x0201 };
 static const
-family_t family_mz  = { "mz",
+family_t family_mz  = { "mz", FAMILY_MZ,
                         80, 0xffc0, 2048, print_mz,  pic32_pemz,  1052, 0x0502 };
 /*
  * This one is a special one for the bootloader. We have no idea what we're
@@ -344,7 +344,7 @@ void mdelay(unsigned msec)
  * Open USB adapter, detected by vendor/product ID.
  * Return a pointer to adapter structure, or 0 when not found.
  */
-static adapter_t *open_usb_adapter(const char *port_name)
+static adapter_t *open_usb_adapter(const char *port_name, int interface, int speed)
 {
     char *delimiter;
     const char *serial = 0;
@@ -353,18 +353,39 @@ static adapter_t *open_usb_adapter(const char *port_name)
     if (!port_name) {
         /* Autodetect the device from a list of known adapters. */
         adapter_t *a = adapter_open_pickit2(0, 0, 0);
-        if (! a)
+		if(a && (INTERFACE_JTAG == interface)){
+			fprintf(stderr, "Found Pickit2, but it does not support the JTAG interface\n");
+			return 0;
+		}
+        if (! a){
             a = adapter_open_pickit3(0, 0, 0);
+			if(a && (INTERFACE_JTAG == interface)){
+				fprintf(stderr, "Found Pickit3, but it does not support the JTAG interface\n");
+				return 0;
+			}
+		}
 #ifdef USE_MPSSE
         if (! a)
-            a = adapter_open_mpsse(0, 0, 0);
+            a = adapter_open_mpsse(0, 0, 0, interface, speed);
 #endif
-        if (! a)
+        if (! a){
             a = adapter_open_hidboot(0, 0, 0);
-        if (! a)
+			if(a && (INTERFACE_DEFAULT != interface)){
+				fprintf(stderr, "Found bootloader, ignoring specified interface\n");
+			}
+		}
+        if (! a){
             a = adapter_open_an1388(0, 0, 0);
-        if (! a)
+			if(a && (INTERFACE_DEFAULT != interface)){
+				fprintf(stderr, "Found bootloader, ignoring specified interface\n");
+			}
+		}
+        if (! a){
             a = adapter_open_uhb(0, 0, 0);
+			if(a && (INTERFACE_DEFAULT != interface)){
+				fprintf(stderr, "Found bootloader, ignoring specified interface\n");
+			}
+		}
         return a;
     }
 
@@ -404,10 +425,16 @@ found:
  * To select other protocols, add a prefix to the port name,
  * like "bitbang:COM5".
  */
-static adapter_t *open_serial_adapter(const char *port_name, int baud_rate)
+static adapter_t *open_serial_adapter(const char *port_name, int baud_rate, 
+										int interface, int speed)
 {
     const char *prefix, *delimiter;
     int prefix_len, len, i;
+
+	if (INTERFACE_DEFAULT != interface){
+		fprintf(stderr, "Non-default interface currently not-supported on \
+						serial adapters, ingoring specified interface\n");
+	}
 
     /* Get protocol prefix. */
     delimiter = strchr(port_name, ':');
@@ -463,7 +490,7 @@ static int is_usb_device(const char *port_name)
 /*
  * Connect to JTAG adapter.
  */
-target_t *target_open(const char *port_name, int baud_rate)
+target_t *target_open(const char *port_name, int baud_rate, int interface, int speed)
 {
     target_t *t;
 
@@ -479,9 +506,9 @@ target_t *target_open(const char *port_name, int baud_rate)
 
     /* Find adapter. */
     if (is_usb_device(port_name)) {
-        t->adapter = open_usb_adapter(port_name);
+        t->adapter = open_usb_adapter(port_name, interface, speed);
     } else {
-        t->adapter = open_serial_adapter(port_name, baud_rate);
+        t->adapter = open_serial_adapter(port_name, baud_rate, interface, speed);
     }
     if (! t->adapter) {
         fprintf(stderr, "\n");
@@ -517,6 +544,8 @@ target_t *target_open(const char *port_name, int baud_rate)
         t->boot_bytes = t->adapter->boot_nbytes;
     }
     t->adapter->family_name = t->family->name;
+    t->adapter->family_name_short = t->family->name_short;
+
     return t;
 }
 

adapter-mpsse.c
@@ -42,7 +42,11 @@ typedef struct {
     uint16_t led_control;
     uint8_t led_inverted;
     const char *product;
-    uint16_t extra_output;
+    uint16_t extra_output;      /* Default OE settings, etc. */
+    uint16_t icsp_control;      /* Selecting ICSP mode. 0 == JTAG, 1 == ICSP */
+    uint16_t icsp_inverted; 
+    uint16_t icsp_oe_control;   /* Selecting data direction in ICSP mode. 0 == OUTPUT, 1 == INPUT */
+    uint16_t icsp_oe_inverted;
 } device_t;
 
 typedef struct {
@@ -59,7 +63,7 @@ typedef struct {
     int bytes_to_write;
 
     /* Receive buffer. */
-    unsigned char input [64];
+    unsigned char input [256];  /* Increased for ICSP, just in case */
     int bytes_to_read;
     int bytes_per_word;
     unsigned long long fix_high_bit;
@@ -71,10 +75,13 @@ typedef struct {
     unsigned trst_control, trst_inverted;
     unsigned sysrst_control, sysrst_inverted;
     unsigned led_control, led_inverted;
+    unsigned icsp_control, icsp_inverted;
+    unsigned icsp_oe_control, icsp_oe_inverted;
     unsigned dir_control;
     unsigned extra_output;
 
     unsigned mhz;
+    unsigned interface;
     unsigned use_executive;
     unsigned serial_execution_mode;
 } mpsse_adapter_t;
@@ -133,21 +140,21 @@ typedef struct {
 #define TMS_HEADER_RESET_TAP_NBITS      6
 #define TMS_HEADER_RESET_TAP_VAL        0b011111
 
-#define TMS_FOOTER_COMMAND_NBITS        3
-#define TMS_FOOTER_COMMAND_VAL          0b001
-#define TMS_FOOTER_XFERDATA_NBITS       3
-#define TMS_FOOTER_XFERDATA_VAL         0b001
+#define TMS_FOOTER_COMMAND_NBITS        2
+#define TMS_FOOTER_COMMAND_VAL          0b01
+#define TMS_FOOTER_XFERDATA_NBITS       2
+#define TMS_FOOTER_XFERDATA_VAL         0b01
 #define TMS_FOOTER_XFERDATAFAST_NBITS   2
 #define TMS_FOOTER_XFERDATAFAST_VAL     0b01
 
 static const device_t devlist[] = {
-    { OLIMEX_VID,           OLIMEX_ARM_USB_TINY,    "Olimex ARM-USB-Tiny",               6,  0x0f10, 0x0100, 1,  0x0200,  0,   0x0800,  0, NULL, 0x0000},
-    { OLIMEX_VID,           OLIMEX_ARM_USB_TINY_H,  "Olimex ARM-USB-Tiny-H",            30,  0x0f10, 0x0100, 1,  0x0200,  0,   0x0800,  0, NULL, 0x0000},
-    { OLIMEX_VID,           OLIMEX_ARM_USB_OCD_H,   "Olimex ARM-USB-OCD-H",             30,  0x0f10, 0x0100, 1,  0x0200,  0,   0x0800,  0, NULL, 0x0000},
-    { OLIMEX_VID,           OLIMEX_MIPS_USB_OCD_H,  "Olimex MIPS-USB-OCD-H",            30,  0x0f10, 0x0100, 1,  0x0200,  1,   0x0800,  0, NULL, 0x0000},
-    { FTDI_DEFAULT_VID,     FTDI_DEFAULT_PID,       "TinCanTools Flyswatter",            6,  0x0cf0, 0x0010, 1,  0x0020,  1,   0x0c00,  1, "Flyswatter", 0x0000},
-    { FTDI_DEFAULT_VID,     FTDI_DEFAULT_PID,       "Neofoxx JTAG/SWD adapter",         30,  0xff3b, 0x0100, 1,  0x0200,  1,   0x8000,  1, "Neofoxx JTAG/SWD adapter", 0x0020},
-    { FTDI_DEFAULT_VID,     FTDI_DEFAULT_PID,       "Dangerous Prototypes Bus Blaster", 30,  0x0f10, 0x0100, 1,  0x0200,  1,   0x0000,  0, NULL, 0x0000},
+    { OLIMEX_VID,           OLIMEX_ARM_USB_TINY,    "Olimex ARM-USB-Tiny",               6,  0x0f10, 0x0100, 1,  0x0200,  0,   0x0800,  0, NULL, 0x0000, 0x0100, 1, 0x0008, 1},
+    { OLIMEX_VID,           OLIMEX_ARM_USB_TINY_H,  "Olimex ARM-USB-Tiny-H",            30,  0x0f10, 0x0100, 1,  0x0200,  0,   0x0800,  0, NULL, 0x0000, 0x0100, 1, 0x0008, 1},
+    { OLIMEX_VID,           OLIMEX_ARM_USB_OCD_H,   "Olimex ARM-USB-OCD-H",             30,  0x0f10, 0x0100, 1,  0x0200,  0,   0x0800,  0, NULL, 0x0000, 0x0100, 1, 0x0008, 1},
+    { OLIMEX_VID,           OLIMEX_MIPS_USB_OCD_H,  "Olimex MIPS-USB-OCD-H",            30,  0x0f10, 0x0100, 1,  0x0200,  1,   0x0800,  0, NULL, 0x0000, 0x0100, 1, 0x0008, 1},
+    { FTDI_DEFAULT_VID,     FTDI_DEFAULT_PID,       "TinCanTools Flyswatter",            6,  0x0cf0, 0x0010, 1,  0x0020,  1,   0x0c00,  1, "Flyswatter", 0x0000, 0x0100, 1, 0x0008, 1},
+    { FTDI_DEFAULT_VID,     FTDI_DEFAULT_PID,       "Neofoxx JTAG/SWD adapter",         30,  0xff3b, 0x0100, 1,  0x0200,  1,   0x8000,  1, "Neofoxx JTAG/SWD adapter", 0x0000, 0x0020, 1, 0x1000, 0},
+    { FTDI_DEFAULT_VID,     FTDI_DEFAULT_PID,       "Dangerous Prototypes Bus Blaster", 30,  0x0f10, 0x0100, 1,  0x0200,  1,   0x0000,  0, NULL, 0x0000, 0x0100, 1, 0x0008, 1},
     { 0 }
 };
 
@@ -389,16 +396,15 @@ static unsigned long long mpsse_recv(mpsse_adapter_t *a)
     return mpsse_fix_data(a, word);
 }
 
-static void mpsse_reset(mpsse_adapter_t *a, int trst, int sysrst, int led)
-{
-    unsigned char buf [3];
-    unsigned output    = 0x0008 | a->extra_output;  /* TCK idle high, OE pins etc. */
-    unsigned direction = 0x000b | a->dir_control;
+/*  Renamed function to mpsse_setPins, since it does more than just reset. 
+ *  Now controls state of reset (always SYSRST), LED,
+ *  ICSP select and ICSP output enable pins */
 
-    if (trst)
-        output |= a->trst_control;
-    if (a->trst_inverted)
-        output ^= a->trst_control;
+static void mpsse_setPins(mpsse_adapter_t *a, int sysrst, int led,
+                            int icsp, int icsp_oe, int immediateWrite)
+{
+    unsigned output    = 0x0008 | a->extra_output;  /* TMS idle high, OE pins etc. */
+    unsigned direction = 0x000b | a->dir_control;
 
     if (sysrst)
         output |= a->sysrst_control;
@@ -410,21 +416,38 @@ static void mpsse_reset(mpsse_adapter_t *a, int trst, int sysrst, int led)
     if (a->led_inverted)
         output ^= a->led_control;
 
+    if (icsp)
+        output |= a->icsp_control;
+    if (a->icsp_inverted)
+        output ^= a->icsp_control;
+
+    if (icsp){
+		if (output & a->icsp_oe_control){
+            output ^= a->icsp_oe_control; /* Fix for boards that use TMS as OE */
+		}
+		if (icsp_oe)
+		    output |= a->icsp_oe_control;
+		if (a->icsp_oe_inverted)
+		    output ^= a->icsp_oe_control;
+	}
+
     /* command "set data bits low byte" */
-    buf [0] = 0x80;
-    buf [1] = output;
-    buf [2] = direction;
-    bulk_write(a, buf, 3);
+    a->output [a->bytes_to_write++] = 0x80;
+    a->output [a->bytes_to_write++] = output;
+    a->output [a->bytes_to_write++] = direction;
 
     /* command "set data bits high byte" */
-    buf [0] = 0x82;
-    buf [1] = output >> 8;
-    buf [2] = direction >> 8;
-    bulk_write(a, buf, 3);
+    a->output [a->bytes_to_write++] = 0x82;
+    a->output [a->bytes_to_write++] = output >> 8;
+    a->output [a->bytes_to_write++] = direction >> 8;
 
-    if (debug_level)
-        fprintf(stderr, "mpsse_reset(trst=%d, sysrst=%d) output=%04x, direction: %04x\n",
-            trst, sysrst, output, direction);
+    if (immediateWrite)
+        mpsse_flush_output(a);
+
+    if (debug_level>1)
+        fprintf(stderr, "mpsse_setPins(sysrst=%d, led=%d, icsp=%d, icsp_oe=%d)\
+                         output=%04x, direction: %04x\n",
+                        sysrst, led, icsp, icsp_oe, output, direction);
 }
 
 static void mpsse_speed(mpsse_adapter_t *a, int khz)
@@ -477,9 +500,9 @@ static void mpsse_close(adapter_t *adapter, int power_on)
     mpsse_flush_output(a);
 
     /* Toggle /SYSRST. */
-    mpsse_reset(a, 0, 1, 1);
+    mpsse_setPins(a, 1, 1, 0, 0, 1); // Reset, LED, no ICSP, no ICSP_OE, immediate
     mdelay(100);    /* Hold in reset for a bit, so it auto-runs afterwards */
-    mpsse_reset(a, 0, 0, 0);
+    mpsse_setPins(a, 0, 0, 0, 0, 1); // No Reset, no LED, no ICSP, no ICSP_OE, immediate
 
     libusb_release_interface(a->usbdev, 0);
     libusb_close(a->usbdev);
@@ -566,7 +589,7 @@ static void serial_execution(mpsse_adapter_t *a)
     }
 
     /* Deactivate /SYSRST. */
-    mpsse_reset(a, 0, 0, 1);
+    mpsse_setPins(a, 0, 1, 0, 0, 1); // No Reset, LED, no ICSP, no ICSP_OE, immediate
     mdelay(10);
 
     /* Check status. */
@@ -1052,7 +1075,8 @@ static void mpsse_verify_data(adapter_t *adapter,
  * When adapter not found, return 0.
  * Parameters vid, pid and serial are not used.
  */
-adapter_t *adapter_open_mpsse(int vid, int pid, const char *serial)
+adapter_t *adapter_open_mpsse(int vid, int pid, const char *serial, 
+                                int interface, int speed)
 {
     mpsse_adapter_t *a;
     unsigned char product [256];
@@ -1094,6 +1118,7 @@ adapter_t *adapter_open_mpsse(int vid, int pid, const char *serial)
             if (match == 1) {
                 a->name = devlist[i].name;
                 a->mhz = devlist[i].mhz;
+                a->interface = interface;
                 a->dir_control      = devlist[i].dir_control;
                 a->trst_control     = devlist[i].trst_control;
                 a->trst_inverted    = devlist[i].trst_inverted;
@@ -1102,6 +1127,10 @@ adapter_t *adapter_open_mpsse(int vid, int pid, const char *serial)
                 a->led_control      = devlist[i].led_control;
                 a->led_inverted     = devlist[i].led_inverted;
                 a->extra_output     = devlist[i].extra_output;
+                a->icsp_control     = devlist[i].icsp_control;
+                a->icsp_inverted     = devlist[i].icsp_inverted;
+                a->icsp_oe_control     = devlist[i].icsp_oe_control;
+                a->icsp_oe_inverted     = devlist[i].icsp_oe_inverted;
                 goto found;
             }
         }
@@ -1168,8 +1197,11 @@ failed: libusb_release_interface(a->usbdev, 0);
     if (debug_level)
         fprintf(stderr, "%s: latency timer: %u usec\n", a->name, latency_timer);
 
-    /* By default, use 500 kHz speed. */
+    /* By default, use 500 kHz speed, unless specified */
     int khz = 500;
+    if (0 != speed){
+        khz = speed;
+    }
     mpsse_speed(a, khz);
 
     /* Disable TDI to TDO loopback. */
@@ -1177,7 +1209,7 @@ failed: libusb_release_interface(a->usbdev, 0);
     bulk_write(a, enable_loopback, 1);
 
     /* Activate LED. */
-    mpsse_reset(a, 0, 0, 1);
+    mpsse_setPins(a, 0, 1, 0, 0, 1); // No Reset, LED, no ICSP, no ICSP_OE, immediate
 
     /* Reset the JTAG TAP controller: TMS 1-1-1-1-1-0.
      * After reset, the IDCODE register is always selected.
@@ -1194,12 +1226,12 @@ failed: libusb_release_interface(a->usbdev, 0);
         if (debug_level > 0 || (idcode != 0 && idcode != 0xffffffff))
             fprintf(stderr, "%s: incompatible CPU detected, IDCODE=%08x\n",
                 a->name, idcode);
-        mpsse_reset(a, 0, 0, 0);
+        mpsse_setPins(a, 0, 0, 0, 0, 1); // Reset, LED, no ICSP, no ICSP_OE, immediate
         goto failed;
     }
 
     /* Activate /SYSRST and LED. */
-    mpsse_reset(a, 0, 1, 1);
+    mpsse_setPins(a, 1, 1, 0, 0, 1); // Reset, LED, no ICSP, no ICSP_OE, immediate
     mdelay(10);
 
     /* Check status. */
@@ -1229,7 +1261,7 @@ failed: libusb_release_interface(a->usbdev, 0);
     if ((status & ~MCHP_STATUS_DEVRST) !=
         (MCHP_STATUS_CPS | MCHP_STATUS_CFGRDY | MCHP_STATUS_FAEN)) {
         fprintf(stderr, "%s: invalid status = %04x\n", a->name, status);
-        mpsse_reset(a, 0, 0, 0);
+        mpsse_setPins(a, 0, 0, 0, 0, 1); // No reset, no LED, no ICSP mode, no ICSP_OE, immediate
         goto failed;
     }
     printf("      Adapter: %s\n", a->name);

adapter.h
@@ -18,6 +18,10 @@
 #define AD_ERASE 0x0004
 #define AD_PROBE 0x0008
 
+#define INTERFACE_DEFAULT   0
+#define INTERFACE_JTAG      1
+#define INTERFACE_ICSP      2
+
 typedef struct _adapter_t adapter_t;
 
 struct _adapter_t {
@@ -29,6 +33,7 @@ struct _adapter_t {
 
     unsigned flags;
     const char *family_name;            /* Name of pic32 family */
+	unsigned family_name_short;			/* Int define of the family name */
 
     void (*close)(adapter_t *a, int power_on);
     unsigned (*get_idcode)(adapter_t *a);
@@ -50,7 +55,7 @@ adapter_t *adapter_open_pickit2(int vid, int pid, const char *serial);
 adapter_t *adapter_open_pickit3(int vid, int pid, const char *serial);
 adapter_t *adapter_open_an1388(int vid, int pid, const char *serial);
 adapter_t *adapter_open_hidboot(int vid, int pid, const char *serial);
-adapter_t *adapter_open_mpsse(int vid, int pid, const char *serial);
+adapter_t *adapter_open_mpsse(int vid, int pid, const char *serial, int interface, int speed);
 adapter_t *adapter_open_bitbang(const char *port, int baud_rate);
 adapter_t *adapter_open_an1388_uart(const char *port, int baud_rate);
 adapter_t *adapter_open_stk500v2(const char *port, int baud_rate);

pic32prog.c
@@ -56,6 +56,8 @@ unsigned boot_bytes;
 unsigned flash_bytes;
 unsigned devcfg_offset;         /* Offset of devcfg registers in boot data */
 int total_bytes;
+int interface = INTERFACE_DEFAULT;  /* Optionally specified JTAG or ICSP */
+int interface_speed = 0;            /* Optional clock speed of interface */
 
 #define devcfg3 (*(unsigned*) &boot_data [devcfg_offset])
 #define devcfg2 (*(unsigned*) &boot_data [devcfg_offset + 4])
@@ -360,7 +362,7 @@ void do_probe()
 {
     /* Open and detect the device. */
     atexit(quit);
-    target = target_open(target_port, target_speed);
+    target = target_open(target_port, target_speed, interface, interface_speed);
     if (! target) {
         fprintf(stderr, _("Error detecting device -- check cable!\n"));
         exit(1);
@@ -441,7 +443,7 @@ int verify_block(target_t *mc, unsigned addr)
 void do_erase()
 {
     atexit(quit);
-    target = target_open(target_port, target_speed);
+    target = target_open(target_port, target_speed, interface, interface_speed);
     if (! target) {
         fprintf(stderr, _("Error detecting device -- check cable!\n"));
         exit(1);
@@ -463,7 +465,7 @@ void do_program(char *filename)
 
     /* Open and detect the device. */
     atexit(quit);
-    target = target_open(target_port, target_speed);
+    target = target_open(target_port, target_speed, interface, interface_speed);
     if (! target) {
         fprintf(stderr, _("Error detecting device -- check cable!\n"));
         exit(1);
@@ -628,7 +630,7 @@ void do_read(char *filename, unsigned base, unsigned nbytes)
 
     /* Open and detect the device. */
     atexit(quit);
-    target = target_open(target_port, target_speed);
+    target = target_open(target_port, target_speed, interface, interface_speed);
     if (! target) {
         fprintf(stderr, _("Error detecting device -- check cable!\n"));
         exit(1);
@@ -750,7 +752,7 @@ int main(int argc, char **argv)
 #endif
     signal(SIGTERM, interrupted);
 
-    while ((ch = getopt_long(argc, argv, "vDhrpeCVWSd:b:B:",
+    while ((ch = getopt_long(argc, argv, "vDhrpeCVWSd:b:B:i:s:",
       long_options, 0)) != -1) {
         switch (ch) {
         case 'v':
@@ -803,6 +805,29 @@ int main(int argc, char **argv)
         case 'S':
             ++skip_verify;
             continue;
+        case 'i':
+            if (strcmp(optarg, "jtag") == 0 || strcmp(optarg, "JTAG") == 0){
+                interface = INTERFACE_JTAG;
+                if (debug_level > 0){
+                    fprintf(stderr, "Using JTAG interface, if available\n");
+                }
+            }
+            else if ( strcmp(optarg, "icsp") == 0 || strcmp(optarg, "ICSP") == 0){
+                interface = INTERFACE_ICSP;
+                if (debug_level > 0){
+                    fprintf(stderr, "Using ICSP interface, if available\n");
+                }
+            }
+            else{
+                fprintf(stderr, "Unknown interface \"%s\" specified\n", optarg);
+                return 0;
+            }
+            continue;
+        case 's':
+            interface_speed = strtoul(optarg, 0, 0);
+            if (debug_level > 0){
+                fprintf(stderr, "Using clock speed of %d khz, if available\n", interface_speed);
+            }
         }
 usage:
         printf("%s.\n\n", copyright);
@@ -826,6 +851,8 @@ usage:
         printf("       -d device           Use specified serial or USB device\n");
         printf("       -b baudrate         Serial speed, default 115200\n");
         printf("       -B alt_baud         Request an alternative baud rate\n");
+        printf("       -i interface        Choose JTAG or ICSP (if supported)\n");
+        printf("       -s clock_speed      Speed of interface in khz, if supported\n");
         printf("       -e                  Erase chip\n");
         printf("       -p                  Leave board powered on\n");
         printf("       -D                  Debug mode\n");

target.h
@@ -17,6 +17,7 @@ typedef void print_func_t(unsigned cfg0, unsigned cfg1, unsigned cfg2, unsigned 
 
 typedef struct {
     const char      *name;
+	unsigned		name_short;
     unsigned        boot_kbytes;
     unsigned        devcfg_offset;
     unsigned        bytes_per_row;
@@ -43,7 +44,7 @@ typedef struct {
     unsigned        boot_bytes;
 } target_t;
 
-target_t *target_open(const char *port, int baud_rate);
+target_t *target_open(const char *port, int baud_rate, int interface, int speed);
 void target_close(target_t *t, int power_on);
 void target_use_executive(target_t *t);
 void target_configure(void);

commit 1f95c6b4cd156464e68bde8568dfeb915f5cf369

Fix regression bug for bootloader mode introduced in 7d512c6e5c7953c301a0bfe6d5a6a168be433555 (Error was that a struct was expanded, but bootloder's struct wasn't updated, making the initial variables be at wrong offsets/places)

pic32.h
@@ -26,6 +26,7 @@ extern const unsigned pic32_pemm_gpl[];
 extern const unsigned pic32_pemm_gpm[];
 extern const unsigned pic32_pemk[];
 
+#define FAMILY_BOOTLOADER	-1
 #define FAMILY_MX1	0
 #define FAMILY_MX3	1
 #define FAMILY_MZ	2

target.c
@@ -60,7 +60,7 @@ family_t family_mk  = { "mk", FAMILY_MK,
  * We don't really care at the end of the day.
  */
 static const
-family_t family_bl  = { "bootloader",
+family_t family_bl  = { "bootloader", FAMILY_BOOTLOADER,
                         80, 0,      1024, 0,         0,           0,    0      };
 
 /*